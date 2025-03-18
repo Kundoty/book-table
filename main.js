b1c0ce18831025b2ee8aa1001f53c878fdcf5c40
Kundoty <banyuanjundao@gmail.com>
2025-03-17 20:43:57 -0400

    fix import resource, display book, author and ISBN

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js'
 const supabaseUrl = 'https://wcwjyplphymvczatwfwy.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indjd2p5cGxwaHltdmN6YXR3Znd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3NDY4NzgsImV4cCI6MjA1NzMyMjg3OH0.CWIKecocxFsUBF96gP1cEC9315DatcdKvQUJxf5uObo'
 const supabase = createClient(supabaseUrl, supabaseKey)
@@ -9,14 +9,10 @@ async function loadBooks() {
     .from('Books')
     .select('*');
 
-    if (error) {
-    console.error('Error fetching books:', error);
-    return;
-    }
-
     const bookList = document.getElementById('booklist');
     for (let book of Books) {
-    bookList.innerHTML += `<li>${book.title}</li>`;
+    bookList.innerHTML += `<li>${book.title} -- ${book.author}</li>`;
+    bookList.innerHTML += `ISBN: ${book.ISBN}<br><br>`;
     }
 }
 
